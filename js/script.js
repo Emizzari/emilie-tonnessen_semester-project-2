bd01422d948e17298b699ac4526e9b1f4e6711a9
Emizzari <emilie@tonnessen.com>
2020-11-18 10:11:14 +0100

    Responsive mobile menu
    Adding and removing class names for an responsive mobile menu

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Mobile menu: --------------------------------- */
+$(document).ready(function(){
+	$('.nav__menu-icon').click(function(){
+		$(this).toggleClass('open');
+
+		if($(this).hasClass('open')) {
+			$('.nav__menu').addClass('open-menu');
+			$('body').addClass('fixed');
+		} else {
+			$('.nav__menu').removeClass('open-menu');
+			$('body').removeClass('fixed');
+		}
+	});
+});
+/* ---------------------------------------------- */
