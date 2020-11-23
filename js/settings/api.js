a71bfc2bc26ae5518c6c8a399711f8449f2b1424
Emizzari <emilie@tonnessen.com>
2020-11-23 10:07:05 +0100

    Adding id import + correcting detailsURL

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,4 +1,6 @@
+import {id} from "../dom/createDetails.js";
+
 export const baseURL = 'http://localhost:1337';
 export const productsURL = baseURL + "/products/";
 export const bannerURL = baseURL + "/home/";
-export const detailsURL = productsURL ;
+export const detailsURL =` ${productsURL}${id}` ;
