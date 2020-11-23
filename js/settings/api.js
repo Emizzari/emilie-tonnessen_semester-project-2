922478c71162c7dcba90735e9fa26a5d6bbddba4
Emizzari <emilie@tonnessen.com>
2020-11-23 14:40:02 +0100

    Styling details

--- js/settings/api.js
+++ js/settings/api.js
@@ -3,4 +3,4 @@ import {id} from '../dom/createDetails.js';
 export const baseURL = 'http://localhost:1337';
 export const productsURL = baseURL + "/products/";
 export const bannerURL = baseURL + "/home/";
-export const detailsURL = `${productsURL}${id}` ;
+export const detailsURL = `${productsURL}${id}`;
