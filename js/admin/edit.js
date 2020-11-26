1371947c437c82d787b8b3deb26c7a7b82991d7f
Emizzari <emilie@tonnessen.com>
2020-11-26 14:02:21 +0100

    Adding image and feature

--- js/admin/edit.js
+++ js/admin/edit.js
@@ -28,15 +28,19 @@ const description = document.querySelector("#description");
 const idInput = document.querySelector("#id");
 const message = document.querySelector(".edit__form__feedback");
 const loading = document.querySelector(".loader");
+const image = document.querySelector("#image");
+const featured = document.querySelector("#featured");
 
 (async function () {
     try {
         const response = await fetch(productUrl);
         const details = await response.json();
 
+        image.value = details.image_url;
         title.value = details.title;
         price.value = details.price;
         description.value = details.description;
+        featured.value = details.featured;
         idInput.value = details.id;
 
         deleteButton(details.id);
@@ -57,16 +61,36 @@ function submitForm(event) {
 
     message.innerHTML = "";
 
+    const imageValue = image.value;
     const titleValue = title.value.trim();
     const priceValue = parseFloat(price.value);
     const descriptionValue = description.value.trim();
+    const featuredValue = featured.value;
     const idValue = idInput.value;
 
+    if (
+        imageValue === 0 ||
+        titleValue.length === 0 || 
+        priceValue.length === 0 || 
+        isNaN(priceValue) || 
+        descriptionValue.length === 0 ||
+        featuredValue === 0
+        ){
+        return displayMessage("feedback feedback--error", "Please supply proper values", ".edit__form__feedback");
     }
 
+    updateProduct(imageValue, titleValue, priceValue, descriptionValue, featuredValue, idValue);
 }
 
+async function updateProduct(image, title, price, description, featured, id) {
     const url = baseURL + "/products/" + id;
+    const data = JSON.stringify({ 
+        image_url: image,
+        title: title, 
+        price: price, 
+        description: description,
+        featured: featured 
+    });
 
     const options = {
         method: "PUT",
@@ -83,10 +107,12 @@ function submitForm(event) {
         console.log(json);
 
         if (json.updated_at) {
+            displayMessage("feedback feedback--success", "Product updated", ".edit__form__feedback");
             fetchAPI(productMenu, productsURL);
         }
 
         if (json.error) {
+            displayMessage("feedback feedback--error", json.message, ".edit__form__feedback");
         }
     } catch (error) {
         console.log(error);
