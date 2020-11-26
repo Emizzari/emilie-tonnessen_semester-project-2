10d84e520ebd8a52aeb6598bc0a9692c928e17ac
Emizzari <emilie@tonnessen.com>
2020-11-26 12:43:42 +0100

    Function: Delete Product button

---
+++ js/admin/deleteButton.js
@@ -0,0 +1,37 @@
+import {baseURL} from "../settings/api.js";
+import {getToken} from "../components/localStorage.js";
+
+export default function deleteButton(id) {
+    const button = document.querySelector(".edit__delete-button");
+
+    button.onclick = async function () {
+        console.log(id);
+
+        const doDelete = confirm("Are you sure you want to delete this?");
+        console.log(doDelete);
+
+        if (doDelete) {
+            const url = baseURL + "/products/" + id;
+
+            const token = getToken();
+
+            const options = {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+
+            try {
+                const response = await fetch(url, options);
+                const json = await response.json();
+
+                location.href = "/admin/edit-product.html";
+
+                console.log(json);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+}
