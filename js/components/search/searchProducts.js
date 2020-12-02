88b3c7636f9a32eaf642c67a31fae6597426fed5
Emizzari <emilie@tonnessen.com>
2020-12-02 07:57:17 +0100

    Adding product description to search value

--- js/components/search/searchProducts.js
+++ js/components/search/searchProducts.js
@@ -7,7 +7,7 @@ export function searchProducts(products) {
         const searchValue = event.target.value;
 
         const filteredProducts = products.filter(function (product) {
-            if (product.title.toLowerCase().includes(searchValue)) {
+            if (product.title.toLowerCase().includes(searchValue), product.description.toLowerCase().includes(searchValue)) {
                 return true;
             }
         });
