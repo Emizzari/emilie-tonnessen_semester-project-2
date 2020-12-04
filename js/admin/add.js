edb3880bb05382ec712cadcbf3065af025facc8b
Emizzari <emilie@tonnessen.com>
2020-12-04 12:56:31 +0100

    importing propper files

--- js/admin/add.js
+++ js/admin/add.js
@@ -1,10 +1,11 @@
 import mobileMenu from "../components/menu/mobile-menu.js";
-import displayMessage from "../components/messages/displayMessage.js";
-import {baseURL, productsURL} from "../components/settings/url.js";
-import {getToken} from "../components/storage/localStorage.js";
-import {fetchAPI} from "../components/settings/fetchAPI.js";
-import {productMenu} from "./components/menu/productMenu.js";
-import { feedback } from "./components/form/containerVariables.js";
+import { productsURL } from "../components/settings/url.js";
+import { token } from "../components/storage/localStorage.js";
+import { fetchAPI } from "../components/settings/fetchAPI.js";
+import { productMenu} from "./components/menu/productMenu.js";
+import logoutButton from "./components/buttons/logoutButton.js";
+import { form } from "./components/form/containerVariables.js";
+import { submitForm } from "./components/form/submitForm.js";
 
 // Redirecting to homepage if they are not logged in
 const token = getToken();
