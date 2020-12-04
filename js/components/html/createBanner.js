ec7493c8c05259f86665e312e21d76f63fc57a66
Emizzari <emilie@tonnessen.com>
2020-12-04 13:00:11 +0100

    formatting code

--- js/components/html/createBanner.js
+++ js/components/html/createBanner.js
@@ -4,4 +4,3 @@ export function createBanner(item){
 	const bannerContainer = document.querySelector(".hero__banner");
 	bannerContainer.style.background=`url("${baseURL}${item.hero_banner.url}")`;
 }
-
