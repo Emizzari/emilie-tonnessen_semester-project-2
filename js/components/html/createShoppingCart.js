564d841c26eb8d8d1098d0118a78e5f31b095dfd
Emizzari <emilie@tonnessen.com>
2020-12-08 13:47:49 +0100

    creating a horizontal shopping cart

--- js/components/html/createShoppingCart.js
+++ js/components/html/createShoppingCart.js
@@ -10,6 +10,7 @@ export function createShoppingCart (){
 
     if (shoppingCart.length === 0) {
         displayMessage(
+            "feedback feedback--warning",
             "The cart is empty", 
             ".cart__products__container"
         );
@@ -20,18 +21,18 @@ export function createShoppingCart (){
         console.log(product);
         cartContainer.innerHTML += `
             <div class="cart__products__card col-12 row">
-                <img src="${product.image}" alt="${product.title}" class="col-sm-12 col-md-4 col-lg cart__products__card__image">
+                <img src="${product.image}" alt="${product.title}" class="col cart__products__card__image">
 
-                <div class="cart__products__card__content col-sm-12 col-lg ">
-                    <h5 class="cart__products__card__title col-sm-12">${product.title}</h5>
-                    <p class="cart__products__card__desc col-sm-12">
+                <div class="cart__products__card__content col ">
+                    <h5 class="cart__products__card__title col">${product.title}</h5>
+                    <p class="cart__products__card__desc col">
                         ${product.description}
                     </p>
                     <a href="${product.url}" class="cart__products__card__link link">
                         View Product <i class="fal fa-angle-right"></i>
                     </a>
                 </div>
-                <p class="cart__products__card__price col-sm">
+                <p class="cart__products__card__price col-sm-12 col-md">
                     ${product.price} NOK
                 </p>
             </div>
