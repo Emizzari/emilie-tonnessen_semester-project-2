04f1a7273f5dc4150dff14203b3816311c640311
Emizzari <emilie@tonnessen.com>
2020-12-09 13:23:37 +0100

    adding redirected search

--- js/index.js
+++ js/index.js
@@ -4,7 +4,10 @@ import { createBanner } from "./components/html/createBanner.js";
 import { createFeaturedProducts } from "./components/html/createFeaturedProducts.js";
 import { fetchAPI } from "./components/settings/fetchAPI.js";
 import countTotalItems from "./components/storage/countStorage.js";
+import { search, redirectUser } from "./components/search/searchUtils.js";
 
 fetchAPI(createBanner, bannerURL);
 fetchAPI(createFeaturedProducts, productsURL);
 countTotalItems();
+
+search.addEventListener("click", redirectUser);
