583981b4d30e6e34362fe846784812630df1f802
Emizzari <emilie@tonnessen.com>
2020-12-11 12:39:16 +0100

    Fixing search bug

--- js/components/search/searchProducts.js
+++ js/components/search/searchProducts.js
@@ -1,16 +1,22 @@
-import { createProduct } from "../../components/html/createProduct.js";
+import { createProduct } from "../html/createProduct.js";
 
 export function searchProducts(products) {
-    const search = document.querySelector(".search");
+    console.log(products);
+    const search = document.querySelector(".search__input");
 
     search.onkeyup = function (event) {
         const searchValue = event.target.value;
 
+        console.log(searchValue);
+
         const filteredProducts = products.filter(function (product) {
-            if (product.title.toLowerCase().includes(searchValue), product.description.toLowerCase().includes(searchValue)) {
+            if (product.title.toLowerCase().includes(searchValue) || 
+                product.description.toLowerCase().includes(searchValue)){
                 return true;
             }
         });
+
+        console.log(filteredProducts);
 
         createProduct(filteredProducts);
     };
